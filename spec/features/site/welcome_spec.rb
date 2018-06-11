761e5044b141402afdeef4f3027afc7e17523c0d
Julian Herrero <microweb10@gmail.com>
2018-06-11 19:35:35 +0700

    add tests for welcome page and title helper

---
+++ spec/features/site/welcome_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.feature "Site#welome", type: :feature, js: true do
+  it "should show the Welcome Page" do
+    visit root_path
+    expect(page).to have_title "Auctions - Welcome Page"
+    expect(page).to have_content "Welcome to Hotels Rooms Auctions"
+  end
+end
