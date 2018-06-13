04d052af487c4480d62401c72599cf176069c33f
Julian Herrero <microweb10@gmail.com>
2018-06-14 01:59:45 +0700

    require rails_helper for test

--- spec/models/user/resend_activation_email_spec.rb
+++ spec/models/user/resend_activation_email_spec.rb
@@ -1,3 +1,5 @@
+require "rails_helper"
+
 RSpec.describe User, type: :model do
   describe "#resend_activation_email!" do
     it "should call the sorcery method send_activation_needed_email! on the user" do
