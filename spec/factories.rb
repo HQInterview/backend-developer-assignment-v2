862d4be2ffb2c0248cc3e805cafd9fe4b8adc1b3
Julian Herrero <microweb10@gmail.com>
2018-06-16 03:00:41 +0700

    user faker and FactoryBot.sequence together to avoid possible duplicates

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
 
   factory :user do
-    sequence(:email) {|n| "user#{n}@miniurl.com" }
+    sequence(:email) {|n| "user#{n}@auctions.com" }
     password "password"
     activation_state "active"
   end
 
   factory :room do
-    sequence(:name) {|n| "Room ##{n}in Hotel #{Faker::Company.name}" }
+    sequence(:name) {|n| "Room ##{n} in Hotel #{Faker::Company.name}" }
     expires_at 10.minutes.from_now
     minimal_bid { rand(10..50) * 100 }
   end
