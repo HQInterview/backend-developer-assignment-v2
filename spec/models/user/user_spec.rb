a4501bc7a876b1faed5e7b9add230d9de1d05c6d
Julian Herrero <microweb10@gmail.com>
2018-06-16 17:35:06 +0700

    add tests for rails model validations

--- spec/models/user/user_spec.rb
+++ spec/models/user/user_spec.rb
@@ -2,6 +2,18 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
 
+  it "should have a CONSTANT named PASSWORD_LENGTH" do
+    expect(User::PASSWORD_LENGTH).to eq 8
+    expect(User::PASSWORD_LENGTH.frozen?).to be true
+  end
+
+  it "should have a CONSTANT named EMAIL_REGEX" do
+    expect(User::EMAIL_REGEX).to eq /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    expect(User::EMAIL_REGEX.frozen?).to be true
+  end
+
+  it { should have_many(:bids).dependent(:destroy) }
+
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
   it { should validate_confirmation_of(:password).with_message("should match the password") }
